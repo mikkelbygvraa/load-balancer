8a41593883573b3f6ab66c595d71025735edf5d9
Mikkel <eaamiby@students.eaaa.dk>
2022-10-29 20:42:09 +0200

    Added Station endpoint

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http.Features;
 using System.Diagnostics;
+using System.Net;
 
 namespace LoadBalancer.Controllers;
 
@@ -18,6 +19,12 @@ public class WeatherForecastController : ControllerBase
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        var hostName = Dns.GetHostName();
+        var ips = Dns.GetHostAddresses(hostName);
+        var ipa = ips.First().MapToIPv4().ToString();
+
+        _logger.LogInformation(1, $"WeatherForecast responding from {ipa}");
     }
 
     // ------------------------------------------------------------------------------
@@ -47,12 +54,39 @@ public class WeatherForecastController : ControllerBase
        
         properties.Add("version", ver);
 
-        var hostName = System.Net.Dns.GetHostName();
-        var ips = await System.Net.Dns.GetHostAddressesAsync(hostName);
+        var hostName = Dns.GetHostName();
+        var ips = await Dns.GetHostAddressesAsync(hostName);
         var ipa = ips.First().MapToIPv4().ToString();
-        
+
         properties.Add("ip-address", ipa);
 
         return properties;
+    }
+
+
+    [HttpGet("station")]
+    public IEnumerable<WeatherForecast> GetStationData(int stationId)
+    {
+        var theData = new List<WeatherForecast>();
+        var clientIP = HttpContext.Connection.RemoteIpAddress?.MapToIPv4();
+
+        if (stationId > 2)
+        {
+            _logger.LogWarning($"Wrong station Id request from {clientIP}");
+        }
+        else
+        {
+            _logger.LogDebug($"Returning weather data for station no. { stationId }");
+
+            return Enumerable.Range(1, 2).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        return theData;
     }
 }
